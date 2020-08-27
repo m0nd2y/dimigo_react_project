d994f2ec52d1fcd06848024799bdb22fa8dac018
m0nd2y <ldj6192@gmail.com>
2020-08-27 09:25:35 +0900

    upload

--- git.py
+++ git.py
@@ -1,4 +1,8 @@
 import os
+print("input commit ")
+commit = raw_input()
 os.system("git add *")
-os.system("git commit -m 'upload'")
+commit_sh = "git commit -m '" + commit + "'"
+print(commit_sh)
+os.system(commit_sh)
 os.system("git push")
