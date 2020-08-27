a31168347f392dc1fdad33f753d2a930728a55d4
m0nd2y <ldj6192@gmail.com>
2020-08-27 09:34:22 +0900

    upload

--- git.py
+++ git.py
@@ -1,6 +1,13 @@
 import os
-print("input commit ")
+
+C_END = "\033[0m"
+C_YELLOW = "\033[33m"
+C_BOLD    = "\033[1m"
+
+
+print(C_BOLD + C_YELLOW + "[+]" + C_END + "put commit : " )
 commit = raw_input()
+
 os.system("git add *")
 commit_sh = "git commit -m '" + commit + "'"
 print(commit_sh)
