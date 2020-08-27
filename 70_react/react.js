c1926a9cbd0370bacade007ceec53bc45eb040fc
m0nd2y <ldj6192@gmail.com>
2020-08-27 09:40:44 +0900

    upload

---
+++ 70_react/react.js
@@ -0,0 +1,6 @@
+// 1. Spread 연산자 (전개 연산자, ES6)
+// 배열
+const num = [1,2,3,4,5];
+console.log(num);
+
+//객체
--- git.py
+++ git.py
@@ -1,15 +1,19 @@
 import os
 
+#color setting
 C_END = "\033[0m"
 C_YELLOW = "\033[33m"
 C_BOLD    = "\033[1m"
 
+#git add
+os.system("git add *")
 
 print(C_BOLD + C_YELLOW + "[+]" + C_END + "put commit : " )
 commit = raw_input()
 
-os.system("git add *")
+#commit
 commit_sh = "git commit -m '" + commit + "'"
-print(commit_sh)
 os.system(commit_sh)
+
+#push
 os.system("git push")
