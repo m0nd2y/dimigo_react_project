4b5ecd75fc0299dda6d40e7441b17555e4b8d76c
m0nd2y <ldj6192@gmail.com>
2020-08-28 16:02:32 +0900

    commit

--- 71_basic/src/App.js
+++ 71_basic/src/App.js
@@ -5,10 +5,25 @@ import Hello2 from './Hello2';
 // 함수형 컴포넌트
 // return (JSX)
 // Hello2 컴포넌트 작성 -> App에 추가
+// JSX 규칙
+// 1. 두개 이상의 태그는 반드시 하나의 태그로 감싸야함
+// 2. 여는 태그와 닫는 태그가 있어야 함
+// 3. JSX안에서 javascript 값을 사용할 때에는 {} 사용함
+// 4. 인라인 style 작성 시 객체 형태로 작성 (Camelcase)
+// 5. css class
+
 
 function App() {
+  const name = "react";
+  const style = {
+    backgroundColor:"yellow", // background-color
+    color: "blue",
+    fontSize : 30, // font-size
+
+  }
   return (
   <div>
+    <div style={style}>{name}</div>
     <Hello />
     <Hello2 />
   </div>
