d99ab02b3382fcfe6af2aaf5da21de7f16d47beb
m0nd2y <ldj6192@gmail.com>
2020-08-28 16:27:29 +0900

    commit

--- 71_basic/src/Hello.js
+++ 71_basic/src/Hello.js
@@ -2,9 +2,12 @@ import React from "react" // const React = react
 
 // const props = { name : "홍길동" , color : "blue", children=""}
 
-function Hello({name, color}) {
+function Hello({name, color, children}) {
     return (
-        <div style={{color}} >안녕하세요, {name}님</div>
+        <>
+            <div style={{color}} >안녕하세요, {name}님</div>
+            <div>{children}</div>
+        </>
     )
 }
 
