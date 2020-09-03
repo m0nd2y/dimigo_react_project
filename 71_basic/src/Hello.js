6db88c8dd73875494340cf346f0b9eb4881eb38f
m0nd2y <ldj6192@gmail.com>
2020-09-03 09:12:23 +0900

    commit

--- 71_basic/src/Hello.js
+++ 71_basic/src/Hello.js
@@ -1,9 +1,10 @@
 import React from "react" // const React = react
 import PropTypes from "prop-types";
 
-// const props = { name : "홍길동" , color : "blue", children=""}
+//  const props = { name : "홍길동" , color : "blue", children=""}
 
 function Hello({ name = "이름없음", color = "black", children = "내용없음"}) {
+    name = "아무개";
     return (
         <>
             <div style={{color}} >안녕하세요, {name}님</div>
