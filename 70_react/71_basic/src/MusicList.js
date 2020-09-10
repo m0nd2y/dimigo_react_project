f75ce337b9f27b8fa69be1b21f1e334bb4f58e36
m0nd2y <ldj6192@gmail.com>
2020-09-10 09:32:21 +0900

    commit

---
+++ 70_react/71_basic/src/MusicList.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function MusicList() {
+    
+    const musicList = [
+        { id: 1, singer: "아이유", title: "eight" },
+        { id: 2, singer: "폴킴", title: "Hero" },
+        { id: 3, singer: "장범준", title: "실버판테온" },
+    ];
+
+    return (
+        <>
+            
+        </>
+    )
+}
+
+
+export default MusicList;
