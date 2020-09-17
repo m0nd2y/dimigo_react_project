52590a0ce88e152bc5939f3afe17a06e8f14c4fe
m0nd2y <ldj6192@gmail.com>
2020-09-17 10:10:07 +0900

    commit

--- 70_react/71_basic/src/MusicList.js
+++ 70_react/71_basic/src/MusicList.js
@@ -16,9 +16,9 @@ function Music({music, onRemoveMusic, onToggleMusic}) {
     useEffect(() => {
         console.log("컴포넌트가 화면에 나타남");
         return () => {
-            console.log("컴포넌트가 화면에서 사라짐");
+            console.log("컴포넌트가 화면에서 사라짐", music);
         };
-    }, [])
+    }, [music])
 
     return (
         <>
