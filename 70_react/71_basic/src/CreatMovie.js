741d7442a9806ea0482d36a311ea6b954cbc1679
m0nd2y <ldj6192@gmail.com>
2020-09-25 15:29:18 +0900

    commit

--- 70_react/71_basic/src/CreatMovie.js
+++ 70_react/71_basic/src/CreatMovie.js
@@ -1,42 +1,42 @@
-import React, {useRef, useContext} from "react";
+import React, { useRef, useContext } from "react";
 import { MovieContext } from "./MovieReducerApp";
 
-function CreatMusic({ mtitle, director, year}) {
-    
-    const dispatch = useContext(MovieContext)
-    
+function CreatMusic({ mtitle, director, year }) {
 
-  const onChangeMovie = (e) => {
-    const { name, value } = e.target;
-    dispatch({
-        type : "CHANGE",
-        name,
-        value
+    const dispatch = useContext(MovieContext)
+
+
+    const onChangeMovie = (e) => {
+        const { name, value } = e.target;
+        dispatch({
+            type: "CHANGE",
+            name,
+            value
         })
     };
 
-  const nextId = useRef(4);
+    const nextId = useRef(4);
 
-  const onCreateMovie = () => {
-      console.log(nextId.current)
-      dispatch({
-          type : "CREATE",
-          movie: {
-              id: nextId.current,
-              mtitle,
-              director,
-              year
+    const onCreateMovie = () => {
+        console.log(nextId.current)
+        dispatch({
+            type: "CREATE",
+            movie: {
+                id: nextId.current,
+                mtitle,
+                director,
+                year
             }
         })
         nextId.current += 1;
         console.log(nextId.current)
-  };
-  
+    };
+
     return (
         <>
-            <input name="mtitle" placeholder = "영화제목" onChange={onChangeMovie} value={mtitle}></input>
-            <input name="director" placeholder = "작가" onChange={onChangeMovie} value={director}></input>
-            <input name="year" placeholder = "년도" onChange={onChangeMovie} value={year}></input>
+            <input name="mtitle" placeholder="영화제목" onChange={onChangeMovie} value={mtitle}></input>
+            <input name="director" placeholder="작가" onChange={onChangeMovie} value={director}></input>
+            <input name="year" placeholder="년도" onChange={onChangeMovie} value={year}></input>
             <button onClick={onCreateMovie}>등록</button>
         </>
     )
